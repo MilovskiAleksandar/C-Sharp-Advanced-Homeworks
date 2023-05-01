2e537fc88f50bc5dce7a85c4f87dd4e6d60a198c
MilovskiAleksandar <119674347+MilovskiAleksandar@users.noreply.github.com>
2023-05-02 00:51:47 +0200

    Homework

---
+++ Homework04/Task1.Domain/DataBase.cs
@@ -0,0 +1,84 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1.Domain
+{
+    public class DataBase<T> where T : Dog
+    {
+        private string _folderPath;
+        private string _filePath;
+
+        public DataBase()
+        {
+            _folderPath = @"..\..\..\Dogs";
+            _filePath = _folderPath + $@"\{typeof(T).Name}s.json";
+
+            if (!Directory.Exists(_folderPath))
+            {
+                Directory.CreateDirectory(_folderPath);
+            }
+
+            if (!File.Exists(_filePath))
+            {
+                File.Create(_filePath).Close();
+            }
+            List<T> data = ReadFromFile();
+            if (data == null)
+            {
+                WriteToFile(new List<T>());
+            }
+
+
+        }
+
+        public List<T> ReadFromFile()
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(_filePath))
+                {
+                    string data = sr.ReadToEnd();
+                    return JsonConvert.DeserializeObject<List<T>>(data);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        public void WriteToFile(List<T> data)
+        {
+            try
+            {
+                string jsonData = JsonConvert.SerializeObject(data);
+                using (StreamWriter sw = new StreamWriter(_filePath))
+                {
+                    sw.WriteLine(jsonData);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public List<T> GetAll()
+        {
+            List<T> data = ReadFromFile();
+            return data;
+        }
+
+        public void Insert(T item)
+        {
+            List<T> data = ReadFromFile();
+            data.Add(item);
+            WriteToFile(data);
+        }
+    }
+}
---
+++ Homework04/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using Task1.Domain;
+
+DataBase<Dog> dogs = new DataBase<Dog>();
+
+List<Dog> readingFromFile = dogs.ReadFromFile();
+foreach (Dog dogsFromFile in readingFromFile)
+{
+    Console.WriteLine($"Name: {dogsFromFile.Name}, Age: {dogsFromFile.Age}, Color: {dogsFromFile.Color}");
+}
+Console.WriteLine("Enter dog name:");
+string inputName = Console.ReadLine();
+
+Console.WriteLine("Enter dog color");
+string inputColor = Console.ReadLine();
+
+Console.WriteLine("Enter dog age");
+int inputAge = Convert.ToInt32(Console.ReadLine());
+
+Dog dog = new Dog(inputName, inputColor, inputAge);
+dogs.Insert(dog);
